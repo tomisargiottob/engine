14b3716eb40257bab652bb5106b05aa701951c5a
Tomas Sargiotto <tomisargiottob@hotmail.com>
2023-01-06 14:48:43 -0300

    feature(app): add database to app

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ import express from "express";
 import config from "config";
 import { initialize } from "express-openapi";
 import logger from "./components/logger";
-import openapiParser from 'swagger-parser'
+import openapiParser from 'swagger-parser';
+import Database from "./components/database";
 
 function errorHandler(err, req, res, next) {
     if (err.status === 400) {
@@ -16,16 +17,20 @@ function errorHandler(err, req, res, next) {
 async function main() {
     const app = express()
     app.use(express.json())
+    const db = new Database({logger},config.db);
+    logger.info('Connecting to database');
+    await db.connect();
     const apiDoc = await openapiParser.dereference('src/api/openapi.yaml')
     initialize({
-        app,
-        apiDoc,
-        dependencies: {
-            logger
-        },
-        errorMiddleware: errorHandler,
-        promiseMode: true,
-        paths: './src/api/controllers'
+      app,
+      apiDoc,
+      dependencies: {
+        logger,
+        db
+      },
+      errorMiddleware: errorHandler,
+      promiseMode: true,
+      paths: './src/api/controllers'
     })
     app.listen(config.port, () => {
         logger.info(`Server up and running on Port ${config.port}`)
