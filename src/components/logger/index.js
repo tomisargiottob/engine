442b30c6385b77276237e1105f418fbb2361ab53
Tomas Sargiotto <tomisargiottob@hotmail.com>
2023-01-05 21:15:38 -0300

    feature(logger): add logger component

---
+++ src/components/logger/index.js
@@ -0,0 +1,14 @@
+import pino from "pino";
+import config from 'config';
+const logger= pino({
+  level: config.logger.level,
+  formatters: {
+    bindings (bindings) {
+      return { pid: bindings.pid, service: 'Engine' }
+    },  
+  },
+  timestamp: () => `,"time":"${new Date(Date.now()).toISOString()}"`
+  
+})
+
+export default logger;
