50c3c2935d1db73745f819192516056c484e3abe
Tomas Sargiotto <tomisargiottob@hotmail.com>
2023-01-08 17:00:08 -0300

    fix(skillsets): fix get skillsets controller

--- src/api/controllers/assistants/{id}/skillsets.js
+++ src/api/controllers/assistants/{id}/skillsets.js
@@ -10,15 +10,16 @@ function skillsetsController(logger, db, errors) {
           throw new errors.NotFoundError('Assistant does not exist')
         }
         log.info('Assistant found')
-        const skillsets = await assistant.getSkillsets(id)
+        const skillsets = await db.skillsets.getAll({assistantId: id})
+        const parsedSkillsets = skillsets.map((skillset) => skillset.toJson())
         res.status(200).send({
-          skillsets
+          skillsets: parsedSkillsets
         })
       } catch (err) {
         if (err instanceof errors.NotFoundError) {  
           res.status(404).send({message: err.message})
         } else {
-          log.error('Could not find assistants skillsets')
+          log.error({reason: err.message },'Could not find assistants skillsets')
           res.status(500).send({
               message: 'Could not get assistants skillsets'
           })
