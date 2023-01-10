6a39bc7c8f601fec6b95d07cbc359e38ee9ff3da
Tomas Sargiotto <tomisargiottob@hotmail.com>
2023-01-10 20:10:24 -0300

    feature(dialogEngine): create converse method and add nodes to dialog

--- src/components/dialogEngine/index.js
+++ src/components/dialogEngine/index.js
@@ -1,17 +1,39 @@
 import createGraph from "ngraph.graph";
+import DialogNode from '../dialogNodes/index'
 
 class Dialog {
-  constructor(dialog) {
-    this.id = dialog.id
-    this.assistantId = dialog.assistantId
+  constructor(skillset) {
+    this.id = skillset.id
+    this.assistantId = skillset.assistantId
     this.dialog = createGraph()
     this.nodes = {}
+    // this.currentDialog = skillset.dialog
+    this.currentDialog = [
+      {name: 'ROOT', response: 'Hello world', condition:'/start', goTo: '', parent: ''},
+      {name: 'Default', response: 'default message', condition:'Anything else', goTo: '', parent: ''},
+      {name: 'first', response: 'Hello world', condition:'Anything else', goTo: '', parent: 'ROOT'}
+    ]
   }
 
-  init(dialog) {
-    for(const node of dialog) {
-      this.nodes[node.name] = new Node(node)
+  init() {
+    for(const node of this.currentDialog) {
+      this.nodes[node.name] = new DialogNode(node)
+      this.dialog.addNode(node.name, node)
+      if(node.parent) {
+        this.dialog.addLink(node.name, node.parent)
+      }
     }
+  }
+
+  converse(message, context) {
+    let lastNodeName = 'ROOT'
+    if(context.lastNode) {
+      lastNodeName = context.lastNode;
+    }
+    const lastNode = this.dialog.getNode(lastNodeName)
+    const nextNodes = this.dialog.getLinks(lastNode);
+    console.log(nextNodes)
+
   }
 }
 
