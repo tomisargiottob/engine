7d55a150235a47731c8b621b282ac93a0cfbd49d
Tomas Sargiotto <tomisargiottob@hotmail.com>
2023-01-10 20:11:17 -0300

    feature(dialogNexus): initialize skillsets and dialogs

--- src/components/dialogNexus/index.js
+++ src/components/dialogNexus/index.js
@@ -1,27 +1,38 @@
+import ContextHandler from "../contextHandler";
 import Dialog from "../dialogEngine";
 
 class DialogNexus {
-  constructor ({db}, config) {
+  constructor ({db, logger}, config) {
     this.dialogs = {}
     this.db = db;
     this.config = config;
+    this.logger = logger.child({module: 'DialogNexus'})
+    this.contextHandler = new ContextHandler({logger: this.logger},config.context)
   }
 
-  init() {
-    const skillsets = this.db.skillsets.getAll();
+  async init() {
+    this.logger.info('Initializing dialog nexus')
+    const skillsets = await this.db.skillsets.getAll();
     for( const skillset of skillsets) {
-      const dialog = new Dialog(skillset)
-      if(this.dialogs[dialog.assistantId]) {
-        this.dialog[dialog.assistantId] = {}
-      }
-      this.dialogs[dialog.assistantId][dialog.id] = dialog;
-      dialog.init();
+      this.initializeDialog(skillset)
     }
   }
+  
+  initializeDialog(skillset) {
+    this.logger.info(`Initializing assistant ${skillset.assistantId}, skillset ${skillset.id}`)
+    const dialog = new Dialog(skillset)
+    if(!this.dialogs[dialog.assistantId]) {
+      this.dialogs[dialog.assistantId] = {}
+    }
+    this.dialogs[dialog.assistantId][dialog.id] = dialog;
+    dialog.init();
+  }
 
-  processResponse(assistantId, skillsetId, message) {
-    console.log(message, assistantId,skillsetId)
-    return 'Hello world'
+  processResponse(assistantId, skillsetId, message, sessionId) {
+    this.logger.info('Processing response')
+    const context = this.contextHandler.getContext(sessionId);
+    const response = this.dialogs[assistantId][skillsetId].converse(message, context)
+    return response || 'Hello world'
   }
 }
 
