eb592c9f3923cdd7076a1f40bda35e9c7b92acd0
Tomas Sargiotto <tomisargiottob@hotmail.com>
2023-02-02 20:57:45 -0300

    feature(controller): add delete and patch controller methods

--- src/api/controllers/assistants/{id}.js
+++ src/api/controllers/assistants/{id}.js
@@ -23,7 +23,56 @@ function assistantController(logger, db, errors) {
           })
         }
       }
-   }
+    },
+    patch: async function updateAssistant(req, res) {
+      const log = logger.child({ module: 'assistantController', method: 'updateAssistant' });
+      try {
+        const {id} = req.params;
+        const { name } = req.body;
+        const assistant = await db.assistants.getById(id)
+        if (!assistant) {
+          log.warn({assistantId: id},'Assistant does not exist')
+          throw new errors.NotFoundError('Assistant does not exist')
+        }
+        log.info('Assistant found')
+        await assistant.update(name)
+        res.status(200).send({
+          assistant: assistant.toJSON()
+        })
+      } catch (err) {
+        if (err instanceof errors.NotFoundError) {  
+          res.status(404).send({message: err.message})
+        } else {
+          log.error({reason: err.message}, 'Could not find assistant')
+          res.status(500).send({
+              message: 'Could not update assistant'
+          })
+        }
+      }
+    },
+    delete: async function removeAssistant(req, res) {
+      const log = logger.child({ module: 'assistantController', method: 'removeAssistant' });
+      try {
+        const {id} = req.params;
+        const assistant = await db.assistants.getById(id)
+        if (!assistant) {
+          log.warn({assistantId: id},'Assistant does not exist')
+          throw new errors.NotFoundError('Assistant does not exist')
+        }
+        log.info('Assistant found')
+        await assistant.remove()
+        res.status(204).send({})
+      } catch (err) {
+        if (err instanceof errors.NotFoundError) {  
+          res.status(404).send({message: err.message})
+        } else {
+          log.error({reason: err.message}, 'Could not find assistant')
+          res.status(500).send({
+              message: 'Could not remove assistant'
+          })
+        }
+      }
+    } 
   };
 }
 
