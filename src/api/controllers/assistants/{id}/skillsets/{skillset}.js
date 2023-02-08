813d1053881f8421e2791d24d341016eb4592b26
Tomas Sargiotto <tomisargiottob@hotmail.com>
2023-02-08 09:12:19 -0300

    fix(controller): fix skillset dialog update method

--- src/api/controllers/assistants/{id}/skillsets/{skillset}.js
+++ src/api/controllers/assistants/{id}/skillsets/{skillset}.js
@@ -34,7 +34,7 @@ function skillsetController(logger, db, errors) {
     patch: async function updateSkillset(req, res) {
       const log = logger.child({ module: 'skillsetController', method: 'updateSkillset' });
       try {
-        const {id, skillsetId} = req.params;
+        const {id, skillset:skillsetId} = req.params;
         const {dialog} = req.body;
         const assistant = await db.assistants.getById(id)
         if (!assistant) {
@@ -47,9 +47,14 @@ function skillsetController(logger, db, errors) {
           log.warn({assistantId: id},'Skillset does not exist')
           throw new errors.NotFoundError('Skillset does not exist')
         }
+        if(dialog && (!dialog.find((node) => node.name === 'ROOT' && !node.parent ) || !dialog.find((node) => node.name === 'Default' && !node.parent ))) {
+          throw new errors.ValidationError('Can not delete Root node')
+        }
         await skillset.update(dialog)
+        const updatedSlillset = await db.skillsets.getOne(skillsetId, id)
+        log.info('Assistant skillset successfully updated')
         res.status(200).send({
-          skillset: skillset.toJSON()
+          skillset: updatedSlillset.toJson()
         })
       } catch (err) {
         if (err instanceof errors.NotFoundError) {  
